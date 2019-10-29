ba52f33ffe2269684350e84a385cea558ac6d419
nazeh <ar.nazeh@gmail.com>
2019-10-29 13:59:19 +0300

    add package-lock.json

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,15 @@ module.exports = {
     rules: [{
       test: /\.scss$/i,
       use: ['style-loader', 'css-loader', 'sass-loader'],
+    }, {
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+        },
+      },
     }],
   },
 };
